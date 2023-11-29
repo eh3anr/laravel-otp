9f4f9c3fd2e1e7696add4711b5a4ab4bc1064ec0
erajabi <erajabi@toranjcapital.com>
2023-11-29 20:14:36 +0330

    edit gitignore

--- src/LaravelOtp.php
+++ src/LaravelOtp.php
@@ -2,7 +2,20 @@
 
 namespace Eh3anr\LaravelOtp;
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Hash;
+
 class LaravelOtp
 {
-    // Build your next great package.
+    public function generateOtp(string $key, int $length = 6, \DateTimeInterface|\DateInterval|int $ttl = 300): int
+    {
+        $otp = random_int(10 ^ $length, 10 ^ ($length + 1) - 1);
+        Cache::put($key, Hash::make($otp), $ttl);
+        return $otp;
+    }
+
+    public function validateOtp()
+    {
+
+    }
 }
