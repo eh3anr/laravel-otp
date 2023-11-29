5835a53516785b27de84c8dabe5397e9db3230ad
erajabi <erajabi@toranjcapital.com>
2023-11-29 21:02:40 +0330

    crate config.php

--- src/LaravelOtp.php
+++ src/LaravelOtp.php
@@ -7,15 +7,26 @@ use Illuminate\Support\Facades\Hash;
 
 class LaravelOtp
 {
+    public function generateCacheKey(string $key): string
+    {
+        return config('otp.cache-prefix', 'otp:') . $key;
+    }
+
     public function generateOtp(string $key, int $length = 6, \DateTimeInterface|\DateInterval|int $ttl = 300): int
     {
         $otp = random_int(10 ^ $length, 10 ^ ($length + 1) - 1);
-        Cache::put($key, Hash::make($otp), $ttl);
+        Cache::put($this->generateCacheKey($key), Hash::make($otp), $ttl);
         return $otp;
     }
 
-    public function validateOtp()
+    public function validateOtp(string $key, string $otp): bool
     {
+        $cachedOtp = Cache::get($this->generateCacheKey($key));
 
+        if (!$cachedOtp) {
+            return false;
+        }
+
+        return Hash::check($otp, $cachedOtp);
     }
 }
